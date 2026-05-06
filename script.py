87032aca95a0bd23ab040b0679eb85e7650776f9
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-05-06 15:07:33 +0700

    Update script.py

--- script.py
+++ script.py
@@ -48,15 +48,15 @@ class Validator:
    css_text=(style.text or "").strip()
    if not css_text:
     continue
-   if "\n\n" in css_text:
-    print("[CSS EMPTY LINE]")
-    self.is_failed=True
-    continue
    try:
     cssutils.parseString(css_text)
    except Exception as e:
     print("[CSS ERROR]")
     print(e)
+    self.is_failed=True
+    continue
+   if "\n\n" in css_text:
+    print("[CSS EMPTY LINE]")
     self.is_failed=True
     continue
   return self.is_failed
